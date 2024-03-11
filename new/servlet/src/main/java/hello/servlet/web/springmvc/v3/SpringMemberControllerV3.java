e9bfe25a7ae87c3074b8b7347bb88deda11709de
CodyKat <piepie1346@gmail.com>
2024-03-12 07:49:42 +0900

    add new project and make v3

--- new/servlet/src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ new/servlet/src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -2,42 +2,42 @@ package hello.servlet.web.springmvc.v3;
 
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/springmvc/v3/members")
+@RequestMapping(value = "/springmvc/v3/members")
 public class SpringMemberControllerV3 {
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @RequestMapping("/new-form")
+//    @RequestMapping(value = "/new-form", method = RequestMethod.GET)
+    @GetMapping("/new-form")
     public String newForm() {
         return "new-form";
     }
 
-    @RequestMapping
-    public ModelAndView members() {
+//    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
+    public String members(Model model) {
         List<Member> members = memberRepository.findAll();
-        ModelAndView mv = new ModelAndView("members");
-        mv.addObject("members", members);
-        return mv;
+        model.addAttribute("members", members);
+        return "members";
     }
 
-    @RequestMapping("/save")
-    public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {
-        String username = request.getParameter("username");
-        int age = Integer.parseInt(request.getParameter("age"));
-
+//    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    @PostMapping("/save")
+    public String save(
+            @RequestParam("username") String username,
+            @RequestParam("age") int age,
+            Model model) {
         Member member = new Member(username, age);
         memberRepository.save(member);
 
-        ModelAndView mv = new ModelAndView("save-result");
-        mv.addObject("member", member);
-        return mv;
+        model.addAttribute("member", member);
+        return "save-result";
     }
 }
